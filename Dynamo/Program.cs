6401ca72209337802d940180ba31dca128c146bb
tpapak01 <tpapak01@gmail.com>
2025-08-12 00:18:29 +0300

    Remove mock GET call

--- Dynamo/Program.cs
+++ Dynamo/Program.cs
@@ -68,35 +68,16 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-// GET API
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
-// POST API
 using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
 ILogger logger = factory.CreateLogger("Program");
 MyAPIs.Initialize<DynamoContext, HouseAliases>(logger);
+// POST API
 MyAPIs.ReceiveEnergyMeasurements(app, "/energydata");
+// GET API
 MyAPIs.SendEnergyPredictions(app, "/energypredictions");
 
 // POST REQUEST EVERY 15 MIN TO ELECTI
 //new MyWorker(dbcontext);
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
